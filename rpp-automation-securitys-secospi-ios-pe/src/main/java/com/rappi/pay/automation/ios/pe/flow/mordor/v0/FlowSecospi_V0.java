1df6fd87c41816e0b69702e86b6ae810a62d787f
juan-carlos-duarte <74014039+juan-carlos-duarte@users.noreply.github.com>
2021-01-19 14:04:47 -0600

    Initial commit

---
+++ rpp-automation-securitys-secospi-ios-pe/src/main/java/com/rappi/pay/automation/ios/pe/flow/mordor/v0/FlowSecospi_V0.java
@@ -0,0 +1,17 @@
+package
+
+com.rappi.pay.automation.ios.pe.flow.mordor.v0;
+
+import com.rappi.pay.automation.mordor.common.dto.SecospiDTO;
+import com.rappi.pay.automation.ios.pe.ui.mordor.v0.UISecospi_V0;
+import io.appium.java_client.AppiumDriver;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FlowSecospi_V0 {
+
+    public boolean stepOne(AppiumDriver<?> driver, SecospiDTO demoDTO) {
+            final UISecospi_V0 uiSecospi = new UISecospi_V0(driver);
+        return true;
+    }
+}
