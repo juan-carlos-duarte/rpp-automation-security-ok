4c9351d2545539dafab4f23ab66b7459f22a23c1
juan-carlos-duarte <74014039+juan-carlos-duarte@users.noreply.github.com>
2021-01-19 14:05:08 -0600

    Initial commit

---
+++ rpp-automation-securitys-secospi-ios-mx/src/main/java/com/rappi/pay/automation/ios/mx/ui/mordor/v0/UISecospi_V0.java
@@ -0,0 +1,19 @@
+package com.rappi.pay.automation.ios.mx.ui.mordor.v0;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+import org.openqa.selenium.support.PageFactory;
+import java.time.Duration;
+
+public class UISecospi_V0 {
+
+    //Selectors
+    @iOSXCUITFindBy(id = "Some selecteor")
+    public static MobileElement text_addAddress;
+
+    public UISecospi_V0(AppiumDriver<?> driver) {
+        PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(10)), this);
+    }
+}
