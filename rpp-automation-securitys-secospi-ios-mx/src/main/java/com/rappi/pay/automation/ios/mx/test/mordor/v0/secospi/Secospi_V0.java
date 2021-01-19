b70779adae7dcd63b456991ffe67467938d7dcf0
juan-carlos-duarte <74014039+juan-carlos-duarte@users.noreply.github.com>
2021-01-19 14:05:09 -0600

    Initial commit

---
+++ rpp-automation-securitys-secospi-ios-mx/src/main/java/com/rappi/pay/automation/ios/mx/test/mordor/v0/secospi/Secospi_V0.java
@@ -0,0 +1,24 @@
+package com.rappi.pay.automation.ios.mx.test.mordor.v0.secospi;
+
+import com.rappi.pay.automation.common.util.BaseTest;
+import com.rappi.pay.automation.mordor.common.dto.SecospiDTO;
+import com.rappi.pay.automation.mordor.common.test.provider.ProviderDTO;
+import com.rappi.pay.automation.ios.mx.flow.mordor.v0.FlowSecospi_V0;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+
+@Slf4j
+public class Secospi_V0 extends BaseTest {
+
+
+    @Test(dataProvider = "SecospiDTO", dataProviderClass = ProviderDTO.class)
+    public void demoStepOne(SecospiDTO demoDTO) {
+
+        log.info("Tu DTO SecospiDTO es el siguiente : " + demoDTO);
+        FlowSecospi_V0 loginFlow = new  FlowSecospi_V0();
+        loginFlow.stepOne(getDriver(), demoDTO);
+
+    }
+
+
+}
